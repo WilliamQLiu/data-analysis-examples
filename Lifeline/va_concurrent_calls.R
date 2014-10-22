a427a62072f25a6a83751a781f812800aab64b42
William Liu <William.Q.Liu@gmail.com>
2014-10-22 14:25:28 -0400

    Fixed order for facet_wrap

--- Lifeline/va_concurrent_calls.R
+++ Lifeline/va_concurrent_calls.R
@@ -7,15 +7,15 @@ library(ggplot2)
 mydata <- read.csv("va_day_hour.csv",skip=0,sep=",",header=FALSE) # Read in CSV
 colnames(mydata)<-c("day", "hour", "calls")  # Rename columns
 
+#myorder <- c("Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat")
+mydata$day <- factor(mydata$day, levels=c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"))
 p <- ggplot(data=mydata, aes(x=hour, y=calls))
-p <- p + geom_point(size=2, alpha=1)
+p <- p + facet_wrap("day", ncol=7)  # Facet by Day
+p <- p + geom_point(size=2, alpha=1/2)
 p <- p + stat_smooth(colour='blue')
 p <- p + xlab("Hour") + ylab("Concurrent Calls")
 p <- p + ggtitle('Concurrent Calls by Day and Hour')
-
-myorder <- c("Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat")
-mydata$day <- factor(mydata$day, levels=myorder)
-p <- p + facet_wrap("day")  # Facet by Day
+p <- p + theme_bw()
 
 p  # Print Plot
-
+ggsave('va_concurrent_calls.png', plot=p)
